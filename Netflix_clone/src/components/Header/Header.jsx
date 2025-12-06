9e691d500e91539a5f32f53157eca9074a19bc63
Samual Molla <sm@MeepleUp.local>
2025-12-06 21:31:12 +0100

    update

--- Netflix_clone/src/components/Header/Header.jsx
+++ Netflix_clone/src/components/Header/Header.jsx
@@ -1,8 +1,38 @@
 import React from 'react'
+import './Header.css'
+import SearchIcon from '@mui/icons-material/Search';
+import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone';
+import AccountBoxIcon from '@mui/icons-material/AccountBox';
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+import netflixlogo from '../../assets/images/netflixlogo.png'
 
 const Header = () => {
-  return (
-    <div>Header</div>
+    return (
+    <div className="outer_header_container">
+        <div className="header_container">
+            <div className="header_left">
+    <ul>
+        <li><img src={netflixlogo} alt="netflix logo"  width='100' />
+        </li>
+        <li>Netflix</li>
+      <li>Home</li>
+      <li>TV Shows</li>
+      <li>Movies</li>
+      <li>New & Popular</li>
+      <li>My List</li>
+      <li>Browse by Languages</li>
+    </ul>
+  </div>
+  <div className="header_right">
+    <ul>
+      <li><SearchIcon/></li>
+      <li><NotificationsNoneIcon/></li>
+      <li><AccountBoxIcon/></li>
+      <li><ArrowDropDownIcon/></li>
+    </ul>
+  </div>
+</div>
+    </div>
   )
 }
 
--- Netflix_clone/src/pages/Home/Home.jsx
+++ Netflix_clone/src/pages/Home/Home.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import Header from '../../components/Header/Header'
+import Footer from '../../components/Footer/Footer'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+      <Header/>
+      <Footer/>
+      
+    </div>
   )
 }
 
