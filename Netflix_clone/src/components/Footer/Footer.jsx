31a5043826971cbf774418c695b64f52526ee414
Samual Molla <sm@MeepleUp.local>
2025-12-06 21:39:19 +0100

    header and footer

--- Netflix_clone/src/components/Footer/Footer.jsx
+++ Netflix_clone/src/components/Footer/Footer.jsx
@@ -1,8 +1,58 @@
 import React from 'react'
+import FacebookIcon from '@mui/icons-material/Facebook';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import XIcon from '@mui/icons-material/X';
+import YouTubeIcon from '@mui/icons-material/YouTube';
+import './footer.css'
 
-const Footer = () => {
+function Footer() {
   return (
-    <div>Footer</div>
+    <div className='footer_outer_container'>
+      <div className='footer_inner_container'>
+        <div className='footer_icons'>
+          <FacebookIcon/>
+          <InstagramIcon/>
+          <XIcon/>
+          <YouTubeIcon/>
+        </div>
+        <div className='footer_lists'>
+          <div>
+            <ul>
+              <li>Audio Description</li>
+              <li>Investor Relations</li>
+              <li>Legal Notices</li>
+            </ul>
+          </div>
+          <div>
+            <ul>
+              <li>Help Center</li>
+              <li>Jobs</li>
+              <li>Cookie Preferences</li>
+            </ul>
+          </div>
+          <div>
+            <ul>
+              <li>Gift Cards</li>
+              <li>Terms of Use</li>
+              <li>Corporate Information</li>
+            </ul>
+          </div>
+          <div>
+            <ul>
+              <li>Media Center</li>
+              <li>Privacy</li>
+              <li>Contact Us</li>
+            </ul>
+          </div>
+          </div>
+        <div className='footer_text'>
+          <p>Netflix GY</p>
+        </div>
+        <div className='copy-write'>
+          <p>© 1997-2024 Netflix, Inc.</p>
+        </div>
+    </div>
+    </div>
   )
 }
 
