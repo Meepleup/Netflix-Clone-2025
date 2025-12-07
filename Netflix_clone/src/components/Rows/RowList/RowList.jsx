728eadf33d0d10a88443a53310ecc7966717a729
Samual Molla <sm@MeepleUp.local>
2025-12-07 12:22:00 +0100

    update all

---
+++ Netflix_clone/src/components/Rows/RowList/RowList.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Row from '../Row/row.jsx'
+
+const RowList = () => {
+  return (
+    <>
+    <Row/>
+    <Row/>
+    <Row/>
+    <Row/>
+    <Row/>
+    </>
+  )
+}
+
+export default RowList
